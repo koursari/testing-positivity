6b9d23fcc0b91edc3a33ae47e7a4104e0dda38e9
koursari <126717841+koursari@users.noreply.github.com>
2023-06-01 13:02:51 +0300

    add guards and name mangling protection

--- modules/subtractor.h
+++ modules/subtractor.h
@@ -1,1 +1,14 @@
+#ifndef SUBTRACTOR_H
+#define SUBTRACTOR_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif //CPLUSPLUS
+
 void subtractor(int x, int y, char *result, size_t buffer_length);
+
+#ifdef __cplusplus
+}
+#endif //CPLUSPLUS
+
+#endif //SUBTRACTOR_H
