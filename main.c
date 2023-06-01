26278a957c9399f2e287429b2133109f8ff82ea1
koursari <126717841+koursari@users.noreply.github.com>
2023-06-01 14:23:24 +0300

    gracefully handle bad (null) input

--- main.c
+++ main.c
@@ -8,6 +8,11 @@ int main()
     char answer[BUFFSIZE] = "";
     subtractor(10, 11, answer, BUFFSIZE);
 
+    if(!answer)
+    {
+        fprintf(stderr, "Unable to access location of answer\n");
+        return 1;
+    }
     printf("%s\n", answer);
     return 0;
 }
